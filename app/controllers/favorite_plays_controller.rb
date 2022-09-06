9c4338913b0f399185e5293e42c5c3689fa63776
toyolife881 <toyolife.island881@gmail.com>
2022-09-07 00:14:49 +0900

    ビュー、コントローラーの修正

--- app/controllers/favorite_plays_controller.rb
+++ app/controllers/favorite_plays_controller.rb
@@ -1,12 +1,12 @@
 class FavoritePlaysController < ApplicationController
   def create
-    FavoritePlay.create(user_id: current_user.id, play_id: params[:id:])
-    redirect_to play_path(params[:id])
+    @favorite_play = FavoritePlay.create(user_id: current_user.id, play_id: params[:play_id])
+    redirect_to play_path(@favorite_play.play_id)
   end
 
   def destroy
-    favorite_play = FavoritePlay.find_by(user_id: current_user.id, play_id: params[:id])
-    favorite_play.destroy
-    redirect_to root_path
+    @favorite_play = FavoritePlay.find_by(user_id: current_user.id, play_id: params[:play_id])
+    @favorite_play.destroy
+    redirect_to play_path(@favorite_play.play_id)
   end
 end
