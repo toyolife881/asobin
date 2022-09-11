b320faded4f2bb9291cb9289b3f936f8a89c10ae
toyolife881 <toyolife.island881@gmail.com>
2022-09-11 22:42:31 +0900

    ビュー、コントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: :show
+  before_action :find_params, only: :show
+
   def show
-    @user = User.find(current_user.id)
+    if current_user.id == @user.id
     @child_ages = @user.user_child_ages
     @plays = @user.play
     @favorite_plays = @user.favorite_play
+    else
+      redirect_to root_path
+    end
   end
+
+  private
+  
+  def find_params
+    @user = User.find(params[:id])
+  end
+
 end
