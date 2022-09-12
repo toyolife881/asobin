26670c36f97fbb96df0ef5c22bc572302a215d1e
toyolife881 <toyolife.island881@gmail.com>
2022-09-12 23:46:21 +0900

    コントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   def show
     if current_user.id == @user.id
     @child_ages = @user.user_child_ages
-    @plays = @user.play
-    @favorite_plays = @user.favorite_play
+    @plays = @user.plays
+    @favorite_plays = @user.favorite_plays
     else
       redirect_to root_path
     end
